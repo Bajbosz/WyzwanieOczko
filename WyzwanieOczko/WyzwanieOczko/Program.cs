c78e6cd21c683e2c8e91086278f4d5d22f64a6a8
bajbo <bajbo@BAJBOSZ>
2024-01-15 18:46:20 +0100

    zad4

--- WyzwanieOczko/WyzwanieOczko/Program.cs
+++ WyzwanieOczko/WyzwanieOczko/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! dddd");
+﻿using System.ComponentModel.Design;
+
+var imie = "Adolf";
+var wiek = 45;
+char plec = 'M';
+
+
+if (plec == 'K' && wiek > 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (imie == "Ewa" && wiek == 30) 
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (plec  ==   'M' && wiek<18) 
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
