1f853ca915827069b7a830fd52d21c7754160878
bajbo <bajbo@BAJBOSZ>
2024-01-18 16:37:33 +0100

    zad dzien 6

---
+++ WyzwanieOczko/WyzwanieOczko/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace WyzwanieOczko
+{
+    internal class Employee
+    {
+        private List<int> score = new List<int>();
+
+        public Employee(string name, string surname, int age ) 
+        {
+            this.Name = name;
+            this.SurName = surname;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public string SurName { get; private set; }
+        public int Age { get; private set;}
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            } 
+        }
+        public void AddScore(int punkt)
+        {
+            this.score.Add(punkt);
+        }
+   }
+
+
+}
--- WyzwanieOczko/WyzwanieOczko/Program.cs
+++ WyzwanieOczko/WyzwanieOczko/Program.cs
@@ -1,68 +1,53 @@
-﻿
-int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-var zero = 0;
-var one = 0;
-var two = 0;
-var three = 0;
-var four = 0;
-var five = 0;
-var six = 0;
-var seven = 0;
-var eight = 0;
-var nine = 0;
-foreach (char c in letters)
-{
-    if (c == '0')
-    {
-        zero++;
-    }
-    else if (c == '1')
-    {
-        one++;
-    }
-    else if (c == '2')
-    {
-        two++;
-    }
-    else if (c == '3')
-    {
-        three++;
-    }
-    else if (c == '4')
-    {
-        four++;
-    }
-    else if (c == '5')
-    {
-        five++;
-    }
-    else if (c == '6')
-    {
-        six++;
-    }
-    else if (c == '7')
-    {
-        seven++;
-    }
-    else if (c == '8')
-    {
-        eight++;
-    }
-    else if (c == '9')
-    {
-        nine++;
-    }
-}
-Console.WriteLine("0=>" + zero);
-Console.WriteLine("1=>" + one);
-Console.WriteLine("2=>" + two);
-Console.WriteLine("3=>" + three);
-Console.WriteLine("4=>" + four);
-Console.WriteLine("5=>" + five);
-Console.WriteLine("6=>" + six);
-Console.WriteLine("7=>" + seven);
-Console.WriteLine("8=>" + eight);
-Console.WriteLine("9=>" + nine);
+﻿using WyzwanieOczko;
 
+Employee user0 = new Employee("Adam", "Moto", 13);
+Employee user1 = new Employee("Antonio", "Moto", 15 );
+Employee user2 = new Employee("Abraham", "Moto", 19 );
+
+// nadawanie wartości pracownikom
+for (int i=0; i<5; i++)
+{
+    Random random = new Random();
+    int numberOfParticipants = 10;
+    int winner = random.Next(1, numberOfParticipants + 1);
+    user0.AddScore(winner);
+}
+for (int i = 0; i < 5; i++)
+{
+    Random random = new Random();
+    int numberOfParticipants = 10;
+    int winner = random.Next(1, numberOfParticipants + 1);
+    user1.AddScore(winner);
+}
+for (int i = 0; i < 5; i++)
+{
+    Random random = new Random();
+    int numberOfParticipants = 10;
+    int winner = random.Next(1, numberOfParticipants + 1);
+    user2.AddScore(winner);
+}
+
+// grupowanie ocen 
+int result0 = user0.Result;
+int result1 = user1.Result;
+int result2 = user2.Result;
+List<int> end = new List<int>();
+end.Add(result0);
+end.Add(result1);
+end.Add(result2);
+// wypisywanie wartości na konsoli
+if ( end.Max() == result0)
+{ 
+    Console.WriteLine(user0.Name +" "+ user0.SurName + " " + user0.Age + " " + user0.Result);
+    Console.WriteLine(end.Max());
+}
+ else if ( end.Max() == result1)
+{ 
+    Console.WriteLine(user1.Name + " " + user1.SurName + "  " + user1.Age + "  " + user1.Result);
+    Console.WriteLine(end.Max());
+}
+else if ( end.Max() == result2)
+{
+    Console.WriteLine(user2.Name + "  " + user2.SurName + "  " + user2.Age + "  " + user2.Result);
+    Console.WriteLine(end.Max());
+}
