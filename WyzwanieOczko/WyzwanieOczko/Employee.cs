52cd1021f1d2adbbd16282101d15adca812369fe
bajbo <bajbo@BAJBOSZ>
2024-01-20 14:31:18 +0100

    poprawki zad

--- WyzwanieOczko/WyzwanieOczko/Employee.cs
+++ WyzwanieOczko/WyzwanieOczko/Employee.cs
@@ -4,7 +4,7 @@
     {
         private List<int> score = new List<int>();
 
-        public Employee(string name, string surname, int age ) 
+        public Employee(string name, string surname, int age)
         {
             this.Name = name;
             this.SurName = surname;
@@ -13,20 +13,20 @@
 
         public string Name { get; private set; }
         public string SurName { get; private set; }
-        public int Age { get; private set;}
+        public int Age { get; private set; }
 
         public int Result
         {
             get
             {
                 return this.score.Sum();
-            } 
+            }
         }
         public void AddScore(int punkt)
         {
             this.score.Add(punkt);
         }
-   }
+    }
 
 
 }
--- WyzwanieOczko/WyzwanieOczko/Program.cs
+++ WyzwanieOczko/WyzwanieOczko/Program.cs
@@ -1,50 +1,39 @@
 ﻿using WyzwanieOczko;
 
 Employee user0 = new Employee("Adam", "Moto", 13);
-Employee user1 = new Employee("Antonio", "Moto", 15 );
-Employee user2 = new Employee("Abraham", "Moto", 19 );
+Employee user1 = new Employee("Antonio", "Moto", 15);
+Employee user2 = new Employee("Abraham", "Moto", 19);
 
-// nadawanie wartości pracownikom
-for (int i=0; i<5; i++)
+List<Employee> PunktList = new List<Employee>()
+{ 
+    user0, user1, user2
+};
+
+foreach (var users in PunktList )
 {
-    Random random = new Random();
-    int numberOfParticipants = 10;
-    int winner = random.Next(1, numberOfParticipants + 1);
-    user0.AddScore(winner);
-}
-for (int i = 0; i < 5; i++)
-{
-    Random random = new Random();
-    int numberOfParticipants = 10;
-    int winner = random.Next(1, numberOfParticipants + 1);
-    user1.AddScore(winner);
-}
-for (int i = 0; i < 5; i++)
-{
-    Random random = new Random();
-    int numberOfParticipants = 10;
-    int winner = random.Next(1, numberOfParticipants + 1);
-    user2.AddScore(winner);
+    for (int i = 0; i < 5; i++)
+    {
+        Random random = new Random();
+        int numberOfParticipants = 10;
+        int winner = random.Next(1, numberOfParticipants + 1);
+        users.AddScore(winner);
+    }
 }
 
-// grupowanie ocen 
-int result0 = user0.Result;
-int result1 = user1.Result;
-int result2 = user2.Result;
-List<int> end = new List<int>();
-end.Add(result0);
-end.Add(result1);
-end.Add(result2);
-// wypisywanie wartości na konsoli
-if ( end.Max() == result0)
-{ 
-    Console.WriteLine(user0.Name +" "+ user0.SurName + " " + user0.Age + " " + user0.Result);
-}
- else if ( end.Max() == result1)
-{ 
-    Console.WriteLine(user1.Name + " " + user1.SurName + "  " + user1.Age + "  " + user1.Result);
-}
-else if ( end.Max() == result2)
+var MaxScoreEmployee = -1;
+
+foreach (var item in PunktList)
 {
-    Console.WriteLine(user2.Name + "  " + user2.SurName + "  " + user2.Age + "  " + user2.Result);
+    if (MaxScoreEmployee < item.Result) 
+    {
+        MaxScoreEmployee = item.Result;        
+    }
+}
+
+foreach (var item in PunktList)
+{
+    if (MaxScoreEmployee == item.Result)
+    {
+        Console.WriteLine(item.Name + " " + item.SurName + " " + item.Age + " " + item.Result);
+    }
 }
